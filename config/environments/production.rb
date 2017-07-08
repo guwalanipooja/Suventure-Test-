84dc02642c1bdeeffaf8b72ec618844d81be759b
Pooja <guwalanipooja@gmail.com>
2017-07-08 14:48:46 +0530

    Add devise email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'suveturetestapp.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
